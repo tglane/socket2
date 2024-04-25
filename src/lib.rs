667eefa9514059dde5edd5a08aaa32ff03bd9a35
Timo Glane <timo.glane@gmail.com>
2024-04-25 17:51:20 +0200

    Removed stuff

--- src/lib.rs
+++ src/lib.rs
@@ -396,23 +396,6 @@ impl RecvFlags {
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
 pub struct TimestampingFlags(sys::c_uint);
 
-const _TARGETS: [&'static str; 14] = [
-    "aarch64-apple-ios",
-    "aarch64-linux-android",
-    "x86_64-apple-darwin",
-    "x86_64-unknown-fuchsia",
-    "x86_64-pc-windows-msvc",
-    "x86_64-pc-solaris",
-    "x86_64-unknown-freebsd",
-    "x86_64-unknown-illumos",
-    "x86_64-unknown-linux-gnu",
-    "x86_64-unknown-linux-musl",
-    "x86_64-unknown-netbsd",
-    "x86_64-unknown-redox",
-    "armv7-linux-androideabi",
-    "i686-linux-android",
-];
-
 #[cfg(any(target_os = "linux", target_os = "android", target_os = "windows"))]
 impl TimestampingFlags {
     /// Creates a new instance of `TimestampingFlags` with no flags set
